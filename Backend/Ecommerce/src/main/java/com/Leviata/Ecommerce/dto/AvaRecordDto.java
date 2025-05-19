c5204a4c5be3866e2d8fbbdcb8c4c51a39dcb531
GustavoHub-ctrl <nevesgustavo2020@gmail.com>
2025-05-19 19:40:29 -0300

    Criação do String comentario e local dtAvaliacao e finalização da classe.

--- Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/dto/AvaRecordDto.java
+++ Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/dto/AvaRecordDto.java
@@ -1,12 +1,14 @@
 package com.Leviata.Ecommerce.dto;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public record AvaRecordDto(
        int id,
        int compraId,
        int nota,
-
-
-
+       String comentario,
+       LocalDateTime dtAvaliacao
 ) {
 }
