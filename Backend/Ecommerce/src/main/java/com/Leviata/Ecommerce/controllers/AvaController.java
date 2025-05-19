f5229f336311a472bd41990b99e0657e7a6903ee
GustavoHub-ctrl <nevesgustavo2020@gmail.com>
2025-05-19 19:46:38 -0300

    Criação do AvaController

---
+++ Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/controllers/AvaController.java
@@ -0,0 +1,14 @@
+package com.Leviata.Ecommerce.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacoes")
+
+@Autowired
+private AvaR
+public class AvaController {
+}
