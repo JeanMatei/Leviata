90d8a53622f72dd4795f3fc34bfec6805522103b
GustavoHub-ctrl <nevesgustavo2020@gmail.com>
2025-05-19 20:05:57 -0300

    PostMapping inserido no codigo

--- Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/controllers/AvaController.java
+++ Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/controllers/AvaController.java
@@ -1,14 +1,28 @@
 package com.Leviata.Ecommerce.controllers;
 
 
+import com.Leviata.Ecommerce.model.AvaModel;
+import com.Leviata.Ecommerce.repositories.AvaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/avaliacoes")
 
-@Autowired
-private AvaR
+
 public class AvaController {
+
+    @Autowired
+    private AvaRepository avaRepository;
+
+    @PostMapping
+    public ResponseEntity<AvaModel>createAva(@RequestBody AvaModel avaModel){
+        AvaModel savedAvamodel = avaRepository.save(avaModel);
+        return new ResponseEntity<>(savedAvamodel, HttpStatus.CREATED);
+    }
 }
