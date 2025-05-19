cbe246b057dafbe474c6b2ad392e5dec6ead7eee
GustavoHub-ctrl <nevesgustavo2020@gmail.com>
2025-05-19 19:53:51 -0300

    Criação dos metodos dentro do repositorio

--- Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/repositories/AvaRepository.java
+++ Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/repositories/AvaRepository.java
@@ -1,4 +1,25 @@
 package com.Leviata.Ecommerce.repositories;
 
-public interface AvaRepository {
+
+import com.Leviata.Ecommerce.model.AvaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AvaRepository extends JpaRepository<AvaModel,Integer> {
+
+    List<AvaModel> findByCompraId( int compraId);
+
+
+    List<AvaModel> findByNota(int nota);
+
+
+    List<AvaModel> findByDataAvaliacaoAfter(LocalDateTime data);
+
+
+    Optional<AvaModel> findById(int id);
 }
