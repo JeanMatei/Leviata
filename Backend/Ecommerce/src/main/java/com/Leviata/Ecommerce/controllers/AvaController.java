7357a46e36e233cb7377f8a20395802a8e7959e1
GustavoHub-ctrl <nevesgustavo2020@gmail.com>
2025-05-19 20:09:35 -0300

    GetMapping inserido no codigo, para listar todas as avaliações

--- Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/controllers/AvaController.java
+++ Backend/Ecommerce/src/main/java/com/Leviata/Ecommerce/controllers/AvaController.java
@@ -6,10 +6,9 @@ import com.Leviata.Ecommerce.repositories.AvaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/avaliacoes")
@@ -25,4 +24,12 @@ public class AvaController {
         AvaModel savedAvamodel = avaRepository.save(avaModel);
         return new ResponseEntity<>(savedAvamodel, HttpStatus.CREATED);
     }
+    @GetMapping
+    public ResponseEntity<List<AvaModel>> getAva(){
+        List<AvaModel> avaliacaoList = avaRepository.findAll();
+        return new ResponseEntity<>(avaliacaoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+
 }
